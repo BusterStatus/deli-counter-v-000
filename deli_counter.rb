154094aa2b9abbedce0b96e34ff1691476ab62e5
matthew.serre <matthew.serre@gmail.com>
2018-10-27 01:48:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,11 @@
 def line(array)
+  sentence = ""
   if array.length == 0
     puts "The line is currently empty."
   else
     array.each do |name|
       string = "#{name.index}. #{name}"
+      sentence = sentence + string
     end
     puts "The line is currently: #{string}"
   end
